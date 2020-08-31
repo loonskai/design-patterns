8aa23910bfc03bd53ad4e6d0a04008401c218756
Siarhei Lunski <loonskai@gmail.com>
2020-08-31 09:06:14 +0300

    Initial commit

---
+++ Behavioral/Observer/NewsLetter.ts
@@ -0,0 +1,89 @@
+import { ISubject, IObserver } from './index';
+
+class NewsLetter implements ISubject {
+  private paperTitle: string;
+
+  public editions = []; /* Subject state */
+
+  private observers: IObserver[] = [];
+
+  constructor(paperTitle: string) {
+    this.paperTitle = paperTitle;
+  }
+
+  public attach(observer: IObserver): void {
+    if (this.observers.includes(observer)) {
+      return console.log('Person is already subscribed for this newsletter.');
+    }
+    this.observers.push(observer);
+  }
+
+  public detach(observer: IObserver): void {
+    if (!this.observers.includes(observer)) {
+      return console.log('Person does not have subscription.');
+    }
+    this.observers = this.observers.filter(subscribedObserver => subscribedObserver !== observer);
+  }
+
+  public notify(): void {
+    this.observers.forEach(subscribedObserver => {
+      subscribedObserver.update(this);
+    })
+  }
+
+  public issueNewEdition(editionTitle: string): void {
+    this.editions.push(editionTitle);
+
+    console.log('---');
+    console.log(`*** ${this.paperTitle} edition: ${editionTitle} ***`);
+    console.log('---');
+
+    this.notify();
+  }
+
+  public getLastEdition() {
+    return this.editions[this.editions.length - 1];
+  }
+}
+
+/* People */
+class Person implements IObserver {
+  private name: string;
+
+  private notification: string;
+
+  constructor(name: string, notification: string) {
+    this.name = name;
+    this.notification = notification;
+  }
+
+  update(newsLetter: NewsLetter) {
+    console.log(`${this.name} -> ${this.notification}: ${newsLetter.getLastEdition()}`);
+  }
+}
+
+/* Create newsletters */
+const WPNewsletter = new NewsLetter('Washington Post');
+const NYTNewsletter = new NewsLetter('New York Times');
+
+/* Create persons */
+const john = new Person('John', 'Here is a new paper for you');
+const jane = new Person('JANE', 'Newspapers update');
+const michael = new Person('Michael', 'GOOD MORNING!');
+
+/* Subscribe */
+WPNewsletter.attach(john);
+WPNewsletter.attach(michael);
+
+NYTNewsletter.attach(jane);
+NYTNewsletter.attach(john);
+
+/* Issue editions */
+NYTNewsletter.issueNewEdition('Tractor and vodka will cure from coronavirus');
+WPNewsletter.issueNewEdition('Protests orchestrated from abroad, criminals involved');
+
+/* Unsubscribe */
+NYTNewsletter.detach(jane);
+NYTNewsletter.detach(john);
+
+NYTNewsletter.issueNewEdition('Last edition of NYT');
---
+++ Behavioral/Observer/index.ts
@@ -0,0 +1,88 @@
+export interface ISubject {
+  attach(observer: IObserver): void;
+  detach(observer: IObserver): void;
+  notify(): void;
+}
+
+export interface IObserver {
+  update(subject: ISubject): void;
+}
+
+class ConcreteSubject implements ISubject {
+  public state: number;
+
+  private observers: IObserver[] = [];
+
+  public attach(observer: IObserver): void {
+    if (this.observers.includes(observer)) {
+      return console.log('Subject: Observer already has been subscribed.');
+    }
+    this.observers.push(observer);
+  }
+
+  public detach(observer: IObserver): void {
+    if (!this.observers.includes(observer)) {
+      return console.log('Subject: Observer does not exist.');
+    }
+    this.observers = this.observers.filter(subscribedObserver => subscribedObserver !== observer);
+  }
+
+  public notify(): void {
+    this.observers.forEach(subscribedObserver => {
+      subscribedObserver.update(this);
+    })
+  }
+
+  public setRandomInt(min: number, max: number): void {
+    const minNum = Math.ceil(min);
+    const maxNum = Math.floor(max);
+    this.state = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+    this.notify();
+  }
+}
+
+class FirstConcreteObserver implements IObserver {
+  update(subject: ISubject) {
+    if (subject instanceof ConcreteSubject && subject.state <= 2) {
+      console.log('FirstConcreteObserver reaction on subject.state change.');
+      console.log('subject.state === ', subject.state);
+      console.log('------');
+    }
+  }
+}
+
+class SecondConcreteObserver implements IObserver {
+  update(subject: ISubject) {
+    if (subject instanceof ConcreteSubject && subject.state > 2) {
+      console.log('SecondConcreteObserver reaction on subject.state change.');
+      console.log('subject.state === ', subject.state);
+      console.log('------');
+    }
+  }
+}
+
+/* Test implementation */
+
+const subject = new ConcreteSubject();
+
+const firstObserver = new FirstConcreteObserver();
+subject.attach(firstObserver);
+
+const secondObserver = new SecondConcreteObserver();
+subject.attach(secondObserver);
+
+subject.setRandomInt(0, 5);
+subject.setRandomInt(0, 5);
+subject.setRandomInt(0, 5);
+subject.setRandomInt(0, 5);
+subject.setRandomInt(0, 5);
+
+console.log('** detach secondObserver **');
+subject.detach(secondObserver)
+
+subject.setRandomInt(0, 5);
+subject.setRandomInt(0, 5);
+subject.setRandomInt(0, 5);
+subject.setRandomInt(0, 5);
+subject.setRandomInt(0, 5);

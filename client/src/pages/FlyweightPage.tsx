dc5033601bd1fc77855d44715e449da5a70e2a33
Siarhei Lunski <loonskai@gmail.com>
2020-11-15 10:24:00 +0300

    Remove game example

--- client/src/pages/FlyweightPage.tsx
+++ client/src/pages/FlyweightPage.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { game } from '../patterns/structural/flyweight';
 
 export default function FlyweightPage(): JSX.Element {
-  console.log(game);
   return (
     <div>
       <h1>Flyweight</h1>
+      <div>TODO</div>
     </div>
   );
 }
--- client/src/patterns/structural/flyweight/constants.ts
+++
@@ -1,5 +0,0 @@
-export const SCENES = {
-  LOAD: 'LOAD_SCENE',
-  MENU: 'MENU_SCENE',
-  GAME: 'GAME_SCENE',
-};

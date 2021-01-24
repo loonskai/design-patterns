fd19c96f5436e922888eb661f7127bd769291384
Siarhei Lunski <loonskai@gmail.com>
2021-01-24 13:23:15 +0300

    Add progress labels

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -9,35 +9,73 @@ export default function Navigation(): JSX.Element {
         <div>
             Creational patterns
           <ul>
-            <li><NavLink to="/abstract-factory">Abstract Factory</NavLink></li>
-            <li><NavLink to="/builder">Builder</NavLink></li>
-            <li><NavLink to="/singleton">Singleton</NavLink></li>
+            <li className="navigation-link">
+              <NavLink to="/abstract-factory">Abstract Factory</NavLink>
+            </li>
+            <li className="navigation-link">
+              <NavLink to="/builder">Builder</NavLink>
+            </li>
+            <li className="navigation-link">
+              <NavLink to="/singleton">Singleton</NavLink>
+            </li>
           </ul>
         </div>
         <div>
             Structural patterns
           <ul>
-            <li><NavLink to="/adapter">Adapter</NavLink></li>
-            <li><NavLink to="/bridge">Bridge</NavLink></li>
-            <li><NavLink to="/decorator">Decorator</NavLink></li>
-            <li><NavLink to="/facade">Facade</NavLink></li>
-            <li><NavLink to="/flyweight">Flyweight</NavLink></li>
-            <li><NavLink to="/proxy">Proxy</NavLink></li>
+            <li className="navigation-link">
+              <NavLink to="/adapter">Adapter</NavLink>
+            </li>
+            <li className="navigation-link">
+              <NavLink to="/bridge">Bridge</NavLink>
+            </li>
+            <li className="navigation-link">
+              <NavLink to="/decorator">Decorator</NavLink>
+            </li>
+            <li className="navigation-link navigation-link__todo">
+              <NavLink to="/facade">Facade</NavLink>
+            </li>
+            <li className="navigation-link">
+              <NavLink to="/flyweight">Flyweight</NavLink>
+            </li>
+            <li className="navigation-link">
+              <NavLink to="/proxy">Proxy</NavLink>
+            </li>
           </ul>
         </div>
         <div>
             Behavioral patterns
           <ul>
-            <li><NavLink to="/chain-of-responsibility">Chain of Responsibility</NavLink></li>
-            <li><NavLink to="/command">Command</NavLink></li>
-            <li><NavLink to="/iterator">Iterator</NavLink></li>
-            <li><NavLink to="/mediator">Mediator</NavLink></li>
-            <li><NavLink to="/memento">Memento</NavLink></li>
-            <li><NavLink to="/observer">Observer</NavLink></li>
-            <li><NavLink to="/state">State</NavLink></li>
-            <li><NavLink to="/strategy">Strategy</NavLink></li>
-            <li><NavLink to="/template-method">Template Method</NavLink></li>
-            <li><NavLink to="/visitor">Visitor</NavLink></li>
+            <li className="navigation-link">
+              <NavLink to="/chain-of-responsibility">Chain of Responsibility</NavLink>
+            </li>
+            <li className="navigation-link navigation-link__in-progress">
+              <NavLink to="/command">Command</NavLink>
+            </li>
+            <li className="navigation-link navigation-link__todo">
+              <NavLink to="/iterator">Iterator</NavLink>
+            </li>
+            <li className="navigation-link navigation-link__todo">
+              <NavLink to="/mediator">Mediator</NavLink>
+            </li>
+            <li className="navigation-link navigation-link__todo">
+              <NavLink to="/memento">Memento</NavLink>
+            </li>
+            <li className="navigation-link navigation-link__todo">
+              <NavLink to="/observer">Observer</NavLink>
+            </li>
+            <li className="navigation-link navigation-link__todo">
+              <NavLink to="/state">State</NavLink>
+            </li>
+            <li className="navigation-link navigation-link__todo">
+              <NavLink to="/strategy">Strategy</NavLink>
+            </li>
+            <li className="navigation-link navigation-link__todo">
+              <NavLink to="/template-method">Template Method</NavLink>
+            </li>
+            <li className="navigation-link navigation-link__todo">
+              <NavLink to="/visitor">Visitor</NavLink>
+            </li>
           </ul>
         </div>
       </nav>
